fc211c650f694da56ed2021ab3f50f3286d1152e
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-10-17 15:47:57 +0700

    Membuat Row pertama

--- app/src/main/java/com/example/myapplication/Tataletak.kt
+++ app/src/main/java/com/example/myapplication/Tataletak.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
@@ -99,6 +100,18 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
         Box(modifier = modifier
             .fillMaxWidth()
             .height(height = 110.dp)
-            .background(color = Color.Yellow), contentAlignment = Alignment.Center))
+            .background(color = Color.Yellow),
+            contentAlignment = Alignment.Center)
+        {
+            Column() {
+                Row(modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly)
+                {
+                    Text(text = "Col1_Row1_Komponen1")
+                    Text(text = "Col1_Row1_Komponen2")
+                    Text(text = "Col1_Row1_Komponen3")
+                }
+            }
+        }
     }
 }
