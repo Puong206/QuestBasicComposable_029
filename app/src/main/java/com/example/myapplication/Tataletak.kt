ff7276bc84cd07c2f037ddbe8bff8155a289c6fc
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-10-17 15:19:37 +0700

    Membuat Column dengan padding 20 dp

--- app/src/main/java/com/example/myapplication/Tataletak.kt
+++ app/src/main/java/com/example/myapplication/Tataletak.kt
@@ -1,2 +1,18 @@
 package com.example.myapplication
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun TataletakColumn(modifier: Modifier) {
+    Columnn(modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
